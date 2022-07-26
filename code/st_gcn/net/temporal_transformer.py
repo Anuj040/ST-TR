fix:attn masking run-off with float16

code/st_gcn/net/temporal_transformer.py
@@ -165,7 +165,10 @@ class TcnUnitAttention(nn.Module):
         # (batch_size, channels, time, joints)
         N, C, T, V = x.size()
         if time_mask is not None:
-            time_mask = -1e9 * time_mask.unsqueeze(-1).tile(1, 1, 1, time_mask.size(-1))
+            time_mask = float("-inf") * time_mask.unsqueeze(-1).tile(
+                1, 1, 1, time_mask.size(-1)
+            )
+            time_mask = torch.nan_to_num(time_mask)
         x_sum = x
 
         if self.data_normalization:

code/st_gcn/net/unit_agcn.py
@@ -90,7 +90,7 @@ class UnitAGCN(nn.Module):
             )
             A2 = self.conv_b[i](x).view(N, self.inter_c * T, V)
             if mask is not None:
-                mask = mask * -1e9
+                mask = torch.nan_to_num(mask * float("-inf"))
                 A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1) + mask)  # N V V
             else:
                 A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1))